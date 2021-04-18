657ed775204d621158bbc92a605802f787c3fd64
KnowYourselves <rsalvarez@uc.cl>
2021-04-18 18:35:54 -0400

    🎉 add default 404 response

--- app.js
+++ app.js
@@ -12,4 +12,18 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.use('/', indexRouter);
 
+// 404
+app.use((req, res) => {
+  res.status(404);
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 module.exports = app;
