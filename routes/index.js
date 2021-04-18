2fe1ae440f00ecb6ca540deff1e868717516233a
KnowYourselves <rsalvarez@uc.cl>
2021-04-18 18:25:06 -0400

    🎉 Add tracks endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const artistsRouter = require('./artists');
 const albumsRouter = require('./albums');
+const tracksRouter = require('./tracks');
 
 router.use('/artists', artistsRouter);
 router.use('/albums', albumsRouter);
+router.use('/tracks', tracksRouter);
 
 module.exports = router;
--- routes/tracks.js
+++ routes/tracks.js
@@ -6,16 +6,40 @@ const router = express.Router();
 * GET REQUESTS
 */
 
-/*
-* POST REQUESTS
-*/
+// GET all tracks.
+router.get('/', async (req, res) => {
+  res.json({
+    endpoint: 'get all tracks',
+  });
+});
+
+// GET an track
+router.get('/:id', async (req, res) => {
+  res.json({
+    endpoint: `get track ${req.params.id}`,
+  });
+});
 
 /*
 * PUT REQUESTS
 */
 
+// PUT (reproduce) a track
+router.put('/:id/play', async (req, res) => {
+  res.json({
+    endpoint: `play the track ${req.params.id}`,
+  });
+});
+
 /*
 * DELETE REQUESTS
 */
+
+// DELETE a track
+router.delete('/:id', async (req, res) => {
+  res.json({
+    endpoint: `delete track ${req.params.id}`,
+  });
+});
 
 module.exports = router;
